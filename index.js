e3cc3863a570e6fc505c6123acbb24f171655463
dyyyylan <jiangdylan00@gmail.com>
2024-10-09 12:24:22 -0400

    a

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const header = document.querySelector("h1");
 console.log(header);
 
-const Domselector = document.querySelector("button");
+
+
+const DOMSelector = {
+    header: document.querySelector("h1"),
+    items: document.querySelector("li"),
+    cardHeader: document.querySelector(".cardheader"),
+    button: document.querySelector(".button"),
+    form: document.querySelector(".form"),
+};
 
 
