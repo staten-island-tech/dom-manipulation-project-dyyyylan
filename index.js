d412132678e22b8be427948f5039deeec370665e
dyyyylan <jiangdylan00@gmail.com>
2024-10-17 12:28:46 -0400

    wdegfrtehrt

--- index.js
+++ index.js
@@ -6,13 +6,28 @@ text: document.querySelector("#text"),
 img: document.querySelector("#img"),
 };
 
-DOMSelectors.form.addEventListener("submit", function (event){
-    event.preventDefault();
+
+function clearinput() {
     DOMSelectors.form.reset();
-    DOMSelectors.parentdiv.insertAdjacentHTML(
-        "beforeend",
-        '<div class="card">    </div>'
-    )
 }
 
-);
+function removeprompt(num) {
+    DOMSelectors.parentdiv.removeChild(`p${num}`);
+}
+
+function addprompt(num) {
+    DOMSelectors.parentdiv.insertAdjacentHTML(
+        "beforeend",
+        `<div class="card" id="p${num}"> <h4>${DOMSelectors.text.value}</h4> <img src ="${DOMSelectors.img.value}"> <button id="b${num}"></button></div>`
+    )
+    document
+    .querySelector(`#b${num}`)
+    .addEventListener("click", function (event){
+        event.preventDefault;
+        document.addEventListener(`#p${num}`).remove();
+    });
+}
+
+function run() {
+    let num = 0;
+}
