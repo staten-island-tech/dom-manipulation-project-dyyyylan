3021db42e9aa6846ea4490fe6c3a7ec7f0987bbd
dyyyylan <jiangdylan00@gmail.com>
2024-10-18 11:27:21 -0400

    d

--- index.js
+++ index.js
@@ -11,32 +11,31 @@ function clearinput() {
     DOMSelectors.form.reset();
 }
 
-function removeprompt(num) {
-    DOMSelectors.parentdiv.removeChild(`p${num}`);
-}
 
 function addprompt(num) {
     DOMSelectors.parentdiv.insertAdjacentHTML(
         "beforeend",
-        `<div class="card" id="p${num}"> <h4>${DOMSelectors.text.value}</h4> <img src ="${DOMSelectors.img.value}"> <button id="b${num}"></button></div>`
+        `<div class="card" id="p${num}"> <h4>${DOMSelectors.text.value}</h4> <img src ="${DOMSelectors.img.value}" class="imgsize"> <button id="b${num}">Remove</button></div>`
     )
     document
     .querySelector(`#b${num}`)
     .addEventListener("click", function (event){
-        event.preventDefault;
+        event.preventDefault();
         document.addEventListener(`#p${num}`).remove();
     });
 }
 
 function run() {
     let n = 0;
-DOMSelectors.submitButton.addEventListener("submit", function (event) {
-    if (!(DOMSelectors.text.value === "" || DOMSelectors.img.value === "")){
-        event.preventDefault();
-        addprompt(n);
-        clearinput();
-    }
-})
+    DOMSelectors.submitButton.addEventListener("click", function (event) {
+        if (!(DOMSelectors.text.value === "" || DOMSelectors.img.value === "")){
+            event.preventDefault();
+            addprompt(n);
+            clearinput();
+            n+= 1;
+        }
+    })
+
 }
 
 run();
